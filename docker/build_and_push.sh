b44a04cf3f7a26e2af56e22c7315bb519fb47604
Granville Matheson <mathesong@gmail.com>
2025-12-10 13:26:17 +0100

    bump version on docker image too

--- docker/build_and_push.sh
+++ docker/build_and_push.sh
@@ -2,7 +2,7 @@
 # Build Docker image from the docker directory
 
 # Set version
-VERSION="v0.0.9"
+VERSION="v0.1.0"
 
 # Build with version tag (build context is parent directory)
 docker build -f Dockerfile -t mathesong/petfit:${VERSION} .. --platform linux/amd64
